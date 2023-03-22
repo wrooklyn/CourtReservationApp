6655e7c00cd948534caa4b5cd94cd8f76311a590
dvylch <chndavide@gmail.com>
2023-03-22 10:36:56 +0100

    appbar: add edit

--- app/src/main/java/it/polito/mad/courtreservationapp/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/courtreservationapp/ShowProfileActivity.kt
@@ -2,6 +2,9 @@ package it.polito.mad.courtreservationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
@@ -29,5 +32,28 @@ class ShowProfileActivity : AppCompatActivity() {
         val pfpElement=findViewById<ImageView>(R.id.imageView3)
         pfpElement.setImageResource(R.drawable.gesu)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.edit -> {
+                editUsername()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun editUsername() {
+        username = "Cristo"
+        val usernameElement = findViewById<TextView>(R.id.username)
+        usernameElement.text = username
     }
 }
