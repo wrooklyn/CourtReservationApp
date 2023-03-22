fd8453da91b7c5ab5cffb943b1ac4aeb15c72eeb
s295334 <s295334@studenti.polito.it>
2023-03-22 11:08:01 +0100

    More work on the layout of the fields in the profile.

--- app/src/main/java/it/polito/mad/courtreservationapp/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/courtreservationapp/ShowProfileActivity.kt
@@ -1,5 +1,4 @@
 package it.polito.mad.courtreservationapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -8,26 +7,46 @@ import java.util.*
 
 enum class Gender{MALE, FEMALE, OTHER}
 class ShowProfileActivity : AppCompatActivity() {
+    var photo : String? = null
     private var username = "Gesú"
-    var firstName = ""
-    var surname = ""
-    var email = ""
-    var password = ""
-    var address = ""
-    var height = 0.0
-    var weight = 0.0
-    var birth = Calendar.getInstance()
-    var gender = Gender.FEMALE
-    var phone = ""
-    var bio = ""
-    var photo:String? = null
+    private var firstName = "Gabri"
+    private var lastName = "Fine"
+    private var email = "email@email.it"
+    private var password = ""
+    private var address = "via carnival in Brazil 3"
+    private var birthDate = Calendar.getInstance()
+
+    private var gender : Gender? = null
+    private var height : Double? = null
+    private var weight : Double? = null
+    private var phone : String?  = null
+    private var bio : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-        val usernameElement = findViewById<TextView>(R.id.username)
-        usernameElement.text = username
+
+        //setting the values of the fields
         val pfpElement=findViewById<ImageView>(R.id.imageView3)
         pfpElement.setImageResource(R.drawable.gesu)
+        val fullNameElement = findViewById<TextView>(R.id.fullname)
+        fullNameElement.text =  "$firstName $lastName"
+        val usernameElement = findViewById<TextView>(R.id.username)
+        usernameElement.text = username
+        val emailElement = findViewById<TextView>(R.id.email)
+        emailElement.text = email
+        val addressElement = findViewById<TextView>(R.id.address)
+        addressElement.text = address
+
+        val genderElement = findViewById<TextView>(R.id.gender)
+        genderElement.text = if(gender != null) gender.toString() else "-"
+        val phoneElement = findViewById<TextView>(R.id.phone)
+        phoneElement.text = if(phone != null) phone else "-"
+        val heightElement = findViewById<TextView>(R.id.height)
+        heightElement.text = if(height != null) "$height cm" else "-"
+        val weightElement = findViewById<TextView>(R.id.weight)
+        weightElement.text = if(weight != null) "$weight cm" else "-"
+        //setting appbar
 
     }
 }
