dee788e061fba2efa926379e2725fbd28b76bdef
dvylch <chndavide@gmail.com>
2023-04-27 09:35:42 +0200

    renamed reservation view model

--- app/src/main/java/it/polito/mad/courtreservationapp/views/reservationManager/ReservationDetailsFragment.kt
+++ app/src/main/java/it/polito/mad/courtreservationapp/views/reservationManager/ReservationDetailsFragment.kt
@@ -1,9 +1,7 @@
 package it.polito.mad.courtreservationapp.views.reservationManager
 
-import android.app.Dialog
 import android.content.Intent
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.drawable.Drawable
 import android.os.Bundle
@@ -13,24 +11,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Recycler
 import it.polito.mad.courtreservationapp.R
 import it.polito.mad.courtreservationapp.db.relationships.ReservationWithServices
 import it.polito.mad.courtreservationapp.db.relationships.ReservationWithSportCenter
-import it.polito.mad.courtreservationapp.models.Court
-import it.polito.mad.courtreservationapp.models.Service
-import it.polito.mad.courtreservationapp.models.User
 import it.polito.mad.courtreservationapp.views.MainActivity
-import it.polito.mad.courtreservationapp.view_model.ReservationFragmentViewModel
 
 class ReservationDetailsFragment : Fragment() {
 
