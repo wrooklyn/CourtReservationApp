5c47600735da38125744d527c0ec5e57cc9d0366
wrooklyn <zhuxiaozhen1@gmail.com>
2023-05-09 01:23:50 +0200

    profileachievements compose view to be adjusted but almost done

--- app/src/main/java/it/polito/mad/courtreservationapp/views/homeManager/tabFragments/ReviewsFragment.kt
+++ app/src/main/java/it/polito/mad/courtreservationapp/views/homeManager/tabFragments/ReviewsFragment.kt
@@ -1,17 +1,51 @@
 package it.polito.mad.courtreservationapp.views.homeManager.tabFragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import it.polito.mad.courtreservationapp.R
+import it.polito.mad.courtreservationapp.db.relationships.CourtWithReviews
+import it.polito.mad.courtreservationapp.db.relationships.SportCenterWithCourtsAndReviews
+import it.polito.mad.courtreservationapp.db.relationships.SportCenterWithCourtsAndServices
+import it.polito.mad.courtreservationapp.models.Court
+import it.polito.mad.courtreservationapp.models.Review
+import it.polito.mad.courtreservationapp.models.User
+import it.polito.mad.courtreservationapp.view_model.SportCenterViewModel
+import it.polito.mad.courtreservationapp.view_model.UserViewModel
+import it.polito.mad.courtreservationapp.views.MainActivity
+import it.polito.mad.courtreservationapp.views.reservationManager.CreateReservationActivity
 
 class ReviewsFragment : Fragment() {
 
+    var position: Int = -1
+    lateinit var viewModel: SportCenterViewModel
+    private lateinit var sportCenterWithCourtsAndServices: SportCenterWithCourtsAndServices
+    lateinit var sportCenterWithCourtsAndReviews: SportCenterWithCourtsAndReviews
+    private var reviews: MutableList<Review> = mutableListOf()
+    private lateinit var courtsWithReviews: List<CourtWithReviews>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        viewModel = (activity as MainActivity).sportCenterViewModel
+        position = requireArguments().getInt("position", -1)
+        sportCenterWithCourtsAndServices = viewModel.sportCentersWithCourtsAndServices[position]
+        sportCenterWithCourtsAndReviews = viewModel.sportCentersWithCourtsAndReviews[position]
+        courtsWithReviews = viewModel.sportCentersWithCourtsAndReviews[position].courtsWithReviews
+//        users = viewModelUser.
+
+        sportCenterWithCourtsAndReviews.courtsWithReviews.forEach(){courtWithReviews ->
+            reviews.addAll(courtWithReviews.reviews)
+        }
     }
 
     override fun onCreateView(
@@ -20,6 +54,47 @@ class ReviewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reviews, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        serviceInitialize()
+    }
+
+    private fun serviceInitialize() {
+        val recyclerView: RecyclerView? = view?.findViewById(R.id.reviewRecycler)
+        val adapter =
+            ReviewsFragment.ReviewAdapter(reviews)
+
+        val llm : LinearLayoutManager = LinearLayoutManager(activity)
+        recyclerView?.layoutManager = llm
+        recyclerView?.isNestedScrollingEnabled=false
+        recyclerView?.adapter = adapter
+    }
+
+    class ReviewAdapter(private val reviews: List<Review>): RecyclerView.Adapter<ReviewViewHolder>() {
+
+        override fun getItemCount()=reviews.size
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+            val itemView=LayoutInflater.from(parent.context).inflate(R.layout.review_item, parent, false)
+            return ReviewViewHolder(itemView)
+        }
+        override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+            reviews.forEach(){review ->
+                holder.bind(review.reviewUserId.toString(), review.text.toString(), review.rating.toFloat())
+            }
+        }
+    }
+
+    class ReviewViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val username: TextView = itemView.findViewById(R.id.userReviewTV)
+        private val reviewText: TextView = itemView.findViewById(R.id.reviewTextTV)
+        private val rating: RatingBar = itemView.findViewById(R.id.ratingBar3)
+        fun bind(usernameData:String, reviewData:String, ratingValue:Float){
+            username.text=usernameData
+            reviewText.text=reviewData
+            rating.rating=ratingValue
+        }
     }
 
     companion object {
--- app/src/main/java/it/polito/mad/courtreservationapp/views/profile/ProfileAchievements.kt
+++ app/src/main/java/it/polito/mad/courtreservationapp/views/profile/ProfileAchievements.kt
@@ -1,23 +1,32 @@
 package it.polito.mad.courtreservationapp.views
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,12 +42,14 @@ import it.polito.mad.courtreservationapp.views.ui.theme.CourtReservationAppTheme
 
 @Composable
 fun AchievementSection(ctx : FragmentActivity?){
-    val sportsPlaceHolder = listOf<String>("Calcio", "Tennis", "Ping Pong")
+    val sportsPlaceHolder = listOf<String>("Soccer", "Tennis", "Ice Skate")
+    val imageId = mapOf("Soccer" to R.drawable.soccer_ball, "Tennis" to R.drawable.tennis, "Ice Skate" to R.drawable.ice_skate)
     Column(modifier = Modifier
         .fillMaxWidth()
         .padding(30.dp)) {
         for(s in sportsPlaceHolder){
-            Text(s)
+            imageId[s]?.let { sportCard(s, it) }
+            Spacer(modifier = Modifier.height(10.dp))
         }
         Button(onClick = {
             val interestId = 1L
@@ -47,6 +58,57 @@ fun AchievementSection(ctx : FragmentActivity?){
             ctx?.startActivity(intent)
         }) {
             Text("+")
+        }
+    }
+}
+
+
+val InterSemiBold = FontFamily(
+    Font(R.font.inter_semibold, FontWeight.Normal)
+)
+val InterRegular = FontFamily(
+    Font(R.font.inter, FontWeight.Normal)
+)
+@Composable
+fun sportCard(sport: String, imageId: Int){
+
+    androidx.compose.material.Card(
+        elevation = 4.dp,
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(size = 12.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier.padding(all = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(size = 62.dp),
+                painter = painterResource(id = imageId),
+                contentDescription = "sport interest  image",
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(width = 8.dp)) // gap between image and text
+            Column(){
+                Text(
+                    text = "$sport - Level 5",
+                    fontSize = 19.sp,
+                    fontFamily = InterSemiBold
+
+                )
+                Spacer(modifier = Modifier.height(height = 5.dp)) // gap between image and text
+                Text(
+                    text = "Won 1st place in local 5k race.",
+                    fontSize = 14.sp,
+                    fontFamily = InterRegular
+
+                )
+
+            }
+
         }
     }
 }
